602ac10f9204f6ec4d80ec27e4ecc4c59bbed30c
Rocco Di Riscio <143715820+rocdir@users.noreply.github.com>
2025-02-10 10:02:13 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Registrar el Service Worker para habilitar la funcionalidad PWA.
+serviceWorkerRegistration.register();
